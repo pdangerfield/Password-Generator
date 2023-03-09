6ec29944202c8ebb21bc21dd1249d67219d57c53
Phil Dangerfield <philipdangerfield@Philips-MBP.askey.com>
2023-03-08 19:51:12 -0700

    created the generatepassword function and var for length, lowercase, uppercase, numbers, special characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
+// Creates the generatePassword function and asks the user how many characters they want the password to be
 function generatePassword(){
-  prompt("How many characters do you want?")
-  var response = prompt;
-  console.log(prompt)
+ var passwordLength = prompt("How many characters (between 8 and 128) do you want?");
+  if(passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)){
+   alert("Your password must be between 8 and 128 characters.");
+   return "";
+}
+
+// Asking what criteria the user wants in their password
+
+var lowercase = confirm("Would you like to include lowercase characters?");
+var uppercase = confirm("would you like to incluce uppercase characters?");
+var numeric = confirm("Would you like to include numbers?");
+var specialCh = confirm("Would you like to include special characters?")
+
+// Check to make sure at least one type of character was selected
+if(!lowercase && !uppercase && !numeric && !specialCh){
+    alert("At least one type of character must be selected.");
+    return "";
+}
+
 }
 // Write password to the #password input
 function writePassword() {
